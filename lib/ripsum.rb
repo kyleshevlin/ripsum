50cec3e643479f3482d82ec811c475ccf6d1c11d
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2015-12-15 19:01:10 -0800

    first pass at adding a configurable library of words

--- lib/ripsum.rb
+++ lib/ripsum.rb
@@ -1,3 +1,18 @@
 require "ripsum/version"
+require "ripsum/configuration"
 require "ripsum/ripsum_helper"
 require "ripsum/railtie"
+
+module Ripsum
+  class << self
+    attr_writer :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+end
