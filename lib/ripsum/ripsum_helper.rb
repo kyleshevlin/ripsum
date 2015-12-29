1af19b1d7674e0c845dbbecbef16a149822fdaed
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2015-12-29 11:38:07 -0800

    default value of variance is 1

--- lib/ripsum/ripsum_helper.rb
+++ lib/ripsum/ripsum_helper.rb
@@ -1,19 +1,25 @@
 module Ripsum
-  def ripsum(standard, variance)
+  def ripsum(standard, variance=1)
     lorem = library
     text = []
 
-    min = standard - (standard * variance)
-    max = standard + (standard * variance)
     lorem_array = lorem.split(' ').shuffle
 
     while standard > lorem_array.length
       lorem_array.concat(lorem_array.shuffle)
     end
 
-    random_number = rand(min..max).floor
+    if variance != 1
+      min = standard - (standard * variance)
+      max = standard + (standard * variance)
+      word_count = rand(min..max).floor
+      word_count
+    else
+      word_count = standard
+      word_count
+    end
 
-    1..random_number.times do |i|
+    1..word_count.times do |i|
       if i == 0
         text << lorem_array[i].capitalize
       else
